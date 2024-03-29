9af9de558379d645f7ef81e02d50a63529689a65
esther-njoroge <nyokabinjorogeesther@gmail.com>
2024-03-29 19:49:30 +0300

    add file

---
+++ functions.js
@@ -0,0 +1,14 @@
+// use a function to reverse all the elements in ascending order
+function reverse (String){
+    const friends= ["John", "Joy", "James", "Jude"]
+}
+console.log(friends.sort());
+console.log(friends.reverse())
+
+function checkElement(){
+    let arr1= arr1.forEach(element =>{
+        if(element>0){
+            console.log("negative")
+        }
+    })
+}
